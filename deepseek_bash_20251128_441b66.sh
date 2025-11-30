b2f741f0d85868efd8c625a6bb7e03e32e9210c1
electrion2023 <128938997+electrion2023@users.noreply.github.com>
2025-11-29 22:47:39 -0300

    Delete deepseek_bash_20251128_441b66.sh

--- deepseek_bash_20251128_441b66.sh
+++
@@ -1,9 +0,0 @@
-# Inicializar git
-git init
-git add .
-git commit -m "Primer commit: sistema de pronóstico automático"
-
-# Conectar con GitHub
-git remote add origin https://github.com/TU_USUARIO/pronostico-automatico-riego.git
-git branch -M main
-git push -u origin main
